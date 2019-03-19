f81eb0a1db76a7af99fcb58848caa34334b08888
github username <github email address>
2019-03-19 18:17:00 -0400

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,30 @@
+INSERT INTO bears (id,name,age,gender,color,temperament,alive) values (1,"Mr. Chocolate
+",12,"male","black","angry",1);
+INSERT INTO bears (id,name,age,gender,color,temperament,alive) values (2,"Rowdy
+",6,"male","white","happy",1);
+INSERT INTO bears (id,name,age,gender,color,temperament,alive) values (3,"Tabitha
+",7,"male","white","angry",1);
+INSERT INTO bears (id,name,age,gender,color,temperament,alive) values (4,"Sergeant Brown
+",2,"male","yellow","angry",1);
+INSERT INTO bears (id,name,age,gender,color,temperament,alive) values (5,"Melissa",7,
+  "female","blue","angry",1);
+INSERT INTO bears (id,name,age,gender,color,temperament,alive) values (6,"Grinch",
+  6,"female","red","angry",1);
+INSERT INTO bears (id,name,age,gender,color,temperament,alive) values (7,"Wendy",
+  3,"female","orange","angry",1);
+INSERT INTO bears (id,name,age,gender,color,temperament,alive) values (8,NULL,4,"male","njnjf","angry",0);
+
+
+
+
+
+
+
+-- it 'has 8 bears' do
+--   expect(@db.execute("SELECT COUNT(*) FROM bears;").flatten[0]).to eq(8)
+-- end
+--
+-- it 'has an unnamed bear' do
+--   expect(@db.execute("SELECT COUNT(*) FROM bears WHERE name IS NULL;").flatten[0]).to eq(1)
+-- end
+-- end
